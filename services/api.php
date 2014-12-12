1035fb66157d5e0ae87a543ccb8a3a6d8fb8f595
navdeep <nsahota@ryerson.ca>
2014-12-11 20:49:09 -0500

    CSS Changes

--- services/api.php
+++ services/api.php
@@ -8,9 +8,9 @@
 	
 		public $data = "";
 		
-		const DB_SERVER = "127.0.0.1";
+		const DB_SERVER = "localhost";
 		const DB_USER = "root";
-		const DB_PASSWORD = "";
+		const DB_PASSWORD = "n2vd33p";
 		const DB = "azdigital";
 
 		// const DB_SERVER = "localhost";
